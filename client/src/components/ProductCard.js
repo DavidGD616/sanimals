5735bd4f16f761128851b3680a5718814472e55d
DavidGD616 <davidguerrerodiaz12@gmail.com>
2024-08-13 17:22:47 -0700

    Product card slide change

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -10,7 +10,7 @@ function ProductCard({ products }) {
                         <img className="peer absolute top-0 right-0 h-full w-full object-cover"
                         src={products.images[0].src} alt={products.title} />
                         <img className="peer absolute top-0 -right-96 h-full w-full object-cover transition-all delay-100 duration-1000 hover:right-0 peer-hover:right-0"
-                        src={products.images[23].src} alt={products.images} />
+                        src={products.images[7].src} alt={products.images} />
                         <svg class="pointer-events-none absolute inset-x-0 bottom-5 mx-auto text-3xl text-white  transition-opacity group-hover:animate-ping group-hover:opacity-30 peer-hover:opacity-0" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M2 10a4 4 0 0 1 4-4h20a4 4 0 0 1 4 4v10a4 4 0 0 1-2.328 3.635a2.996 2.996 0 0 0-.55-.756l-8-8A3 3 0 0 0 14 17v7H6a4 4 0 0 1-4-4V10Zm14 19a1 1 0 0 0 1.8.6l2.7-3.6H25a1 1 0 0 0 .707-1.707l-8-8A1 1 0 0 0 16 17v12Z" /></svg>
                     
                 </div>
