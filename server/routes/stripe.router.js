ad16890958faaea1af061a2f692f51adb8dbd3c5
DavidGD616 <davidguerrerodiaz12@gmail.com>
2024-08-18 16:32:56 -0700

    stripe router

---
+++ server/routes/stripe.router.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const Stripe = require('stripe');
+require('dotenv').config();
+
+const router = express.Router();
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+router.post('/create-checkout-session', async (req, res) => {
+    const { cartItems } = req.body;
+
+    const lineItems = cartItems.map(item => ({
+        price_data: {
+            currency: 'usd',
+            product_data: {
+                name: item.title,
+                images: item.image,
+            },
+            unit_amount: item.price,
+        },
+        quantity: item.quantity
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: `${process.env.CLIENT_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.CLIENT_URL}/cancel`,
+    });
+
+    res.json({ id: session.id })
+})
