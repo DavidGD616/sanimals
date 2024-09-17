ed6b7820f526089445d7592dc661176d6d944516
DavidGD616 <davidguerrerodiaz12@gmail.com>
2024-09-16 19:25:34 -0700

    fetch route updated

--- client/src/index.js
+++ client/src/index.js
@@ -8,13 +8,13 @@ import { CartProvider } from './context/CartProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <NextUIProvider>
       <CartProvider>
         <App />
       </CartProvider>
     </NextUIProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
